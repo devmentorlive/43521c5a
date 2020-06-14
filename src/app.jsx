b14df35f7ad0708506877f652df800b4d15b84a8
Mark Tellez <mark.tellez@icloud.com>
2020-06-14 08:31:17 -0500

    structured solve

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <img src="/images/stars/green.png" />
+      <img src="/images/stars/green.png" />
+      <img src="/images/stars/green.png" />
+      <img src="/images/stars/green.png" />
+      <img src="/images/stars/green.png" />
+    </div>
+  );
+}
